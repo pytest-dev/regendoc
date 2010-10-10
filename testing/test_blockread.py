91d64ff56004a00763a0f021f7edc2741f0f120d
Ronny Pfannschmidt <Ronny.Pfannschmidt@gmx.de>
2010-10-10 22:16:22 +0200

    less fine grained blocks

--- testing/test_blockread.py
+++ testing/test_blockread.py
@@ -9,7 +9,7 @@ next block::
 
     indent
     block
-    
+
     with more
 
 .. directive:: test
@@ -22,13 +22,10 @@ def test_blocks():
     result = blocks(input_data.splitlines(True))
     expected = [
         # indent level, start line, lines
-        (0, 1, ['some text\n', 'more text\n']),
-        (0, 4, ['next block::\n']),
-        (4, 6, ['indent\n', 'block\n']),
-        (4, 9, ['with more\n']),
+        (0, 0, ['some text\n', 'more text\n', '\n', 'next block::\n']),
+        (4, 6, ['indent\n', 'block\n', '\n', 'with more\n']),
         (0, 11, ['.. directive:: test\n']),
-        (4, 12, [':param: test\n']),
-        (4, 14, ['text\n']),
+        (4, 12, [':param: test\n', '\n', 'text\n'])
     ]
     pprint.pprint(result)
     assert result == expected
