216db1e5ee40b92694ed31e1267cf90cbc42fe7d
Ronny Pfannschmidt <Ronny.Pfannschmidt@gmx.de>
2010-10-10 22:44:15 +0200

    add a simple block classification

---
+++ simpledoctest/classify.py
@@ -0,0 +1,15 @@
+
+
+def classify(lines):
+    first = lines[0]
+    content = ''.join(lines[1:])
+    if first.startswith('# content of'):
+        target = first.strip().split()[-1]
+        return 'write', target, content
+    elif first[0] == '$':
+        cmd = first[1:].strip()
+        return 'exec', cmd, content
+
+    return None, first, content
+
+    
