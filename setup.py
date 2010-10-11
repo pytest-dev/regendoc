06619f73f42c95b3d5d554e775f72656097567c9
Ronny Pfannschmidt <opensource@ronnypfannschmidt.de>
2010-10-11 13:23:16 +0200

    add description

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from setuptools import setup
 setup(
     name='RegenDoc',
     version='0.1',
+    description='a tool to check/fix simple file/shell examples'
+                ' in documentation',
     url='http://bitbucket.org/RonnyPfannschmidt/regendoc/',
     author='Ronny Pfannschmidt',
     author_email='Ronny.Pfannschmidt@gmx.de',
