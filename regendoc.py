865419f129500e52fe4a3ab2eaf201bf9ba35723
Ronny Pfannschmidt <Ronny.Pfannschmidt@gmx.de>
2010-10-11 08:40:20 +0200

    inline simpledoctest.classify in regendoc.py

--- regendoc.py
+++ regendoc.py
@@ -17,6 +17,30 @@ parser.add_argument('files',
                    help='the files to check/update')
 
 
+def classify(lines, indent=4, line=None):
+    first = lines[0]
+    content = ''.join(lines[1:])
+
+    def at(action, target):
+        return {
+            'action': action,
+            'target': target,
+            'content': content,
+            'indent': indent,
+            'line': line,
+        }
+
+
+    if first.startswith('# content of'):
+        target = first.strip().split()[-1]
+        return at('write', target)
+    elif first[0] == '$':
+        cmd = first[1:].strip()
+        return at('shell', cmd)
+
+    return at(None, first)
+
+
 def actions_of(file):
     lines = file.read().splitlines(True)
     for indent, line, data in blocks(lines):
