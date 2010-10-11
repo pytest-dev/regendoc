06054c7e61eb6ed296d40b0eb143d9b8daeb7ce2
Ronny Pfannschmidt <opensource@ronnypfannschmidt.de>
2010-10-11 08:55:50 +0200

    inline executor tests

--- test_regendoc.py
+++ test_regendoc.py
@@ -1,6 +1,13 @@
-import pprint
-from regendoc import blocks
+
+import py
+from regendoc import blocks, correct_content
 from regendoc import classify
+from regendoc import execute, actions_of
+
+from operator import itemgetter
+a_c_t = itemgetter('action', 'target')
+
+example = py.path.local(__file__).dirpath().join('example.txt')
 
 input_data_for_blocks = """
 some text
@@ -19,6 +26,13 @@ next block::
     text
 """
 
+simple = """
+an echo:
+    $ echo hi
+    oh no
+"""
+
+simple_corrected = simple.replace('oh no', 'hi')
 
 def test_blocks():
     result = blocks(input_data_for_blocks.splitlines(True))
@@ -29,7 +43,7 @@ def test_blocks():
         (0, 11, ['.. directive:: test\n']),
         (4, 12, [':param: test\n', '\n', 'text\n'])
     ]
-    pprint.pprint(result)
+    py.std.pprint.pprint(result)
     assert result == expected
 
 
@@ -64,3 +78,63 @@ def test_classify_shell():
         'line': None,
     }
     assert cmd == expected
+
+
+def test_simple_new_content(tmpdir):
+    fp = tmpdir.join('example.txt')
+    fp.write(simple)
+    needed_update ,= execute(
+        file = fp,
+        tmpdir = tmpdir,
+    )
+
+    expeccted_update = {
+        'action': 'shell',
+        'target': 'echo hi',
+        'content': 'oh no\n',
+        'new_content': 'hi\n',
+        'indent': 4,
+        'line': 2,
+    }
+    assert needed_update == expeccted_update
+
+
+def test_single_update():
+
+    update = {
+        'action': 'shell',
+        'target': 'echo hi',
+        'content': 'oh no\n',
+        'new_content': 'hi\n',
+        'indent': 4,
+        'line': 2,
+    }
+
+    corrected = correct_content(simple, [update])
+    assert corrected == simple_corrected
+
+
+def test_execute_actions(tmpdir):
+
+
+    actions = list(actions_of(example))
+
+    interesting =  [a_c_t(x) for x in actions]
+    expected = [
+        ('write', 'test_simplefactory.py'),
+        ('shell', 'py.test test_simplefactory.py')]
+
+    assert interesting == expected
+
+
+def test_execute_run(tmpdir):
+
+    execute(
+        file=example,
+        tmpdir=tmpdir,
+    )
+    assert tmpdir.join('test_simplefactory.py').check()
+
+
+
+
