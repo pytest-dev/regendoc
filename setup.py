68d660721e0150b2b9e6a2d884ee0495efc6f655
Ronny Pfannschmidt <Ronny.Pfannschmidt@gmx.de>
2010-10-11 10:08:04 +0200

    add seup.py, .hgignore and manifest

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='RegenDoc',
+    version='0.1',
+    url='http://bitbucket.org/RonnyPfannschmidt/regendoc/',
+    author='Ronny Pfannschmidt',
+    author_email='Ronny.Pfannschmidt@gmx.de',
+    py_modules=['regendoc'],
+)
+
