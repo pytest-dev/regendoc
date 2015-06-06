cab0a75b12badf20a69100de68ae6ce77a8390e6
Ronny Pfannschmidt <opensource@ronnypfannschmidt.de>
2015-06-06 15:26:41 +0200

    move printdiff code

--- regendoc/__init__.py
+++ regendoc/__init__.py
@@ -5,70 +5,7 @@ import subprocess
 import shutil
 
 from .parse import parse_actions, correct_content
-
-
-def write(name, targetdir, action):
-    # XXX: insecure
-    target = os.path.join(targetdir, action['target'])
-    targetdir = os.path.dirname(target)
-    if not os.path.isdir(targetdir):
-        os.makedirs(targetdir)
-    with open(target, 'w') as fp:
-        fp.write(action['content'])
-
-
-def shell(name, targetdir, action):
-    if action['cwd']:
-        cwd = os.path.join(targetdir, action['cwd'])
-    else:
-        cwd = targetdir
-
-    proc = subprocess.Popen(
-        action['target'],
-        shell=True,
-        cwd=cwd,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.STDOUT,
-        bufsize=0,
-    )
-    out, err = proc.communicate()
-    out = out.decode('utf-8')
-    assert not err
-    if out != action['content']:
-        import difflib
-        differ = difflib.Differ()
-        outl = out.splitlines(True)
-        contl = action['content'].splitlines(True)
-        result = differ.compare(contl, outl)
-        printdiff(result)
-        return out
-
-
-def wipe(name, targetdir, action):
-    click.secho('wiping targetdir %s of %s' % (targetdir, name), bold=True)
-    shutil.rmtree(targetdir)
-    os.mkdir(targetdir)
-
-
-ACTIONS = {
-    'shell': shell,
-    'wipe': wipe,
-    'write': write,
-}
-
-
-def printdiff(lines):
-    mapping = {
-        '+': 'green',
-        '-': 'red',
-        '?': 'blue',
-    }
-    for line in lines:
-        color = mapping.get(line[0])
-        if color:
-            click.secho(line, fg=color, bold=True, nl=False)
-        else:
-            click.echo(line, nl=False)
+from .actions import ACTIONS
 
 
 def check_file(name, content, tmpdir):
